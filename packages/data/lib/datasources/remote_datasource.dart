65b4a8b2fdaab1ac071e5052abcc2dfb0cddfddc
Dmitro Serdun <d.serdun@webtrit.com>
2024-06-03 01:37:29 +0300

    fix(data): use correct property for parce data

--- packages/data/lib/datasources/remote_datasource.dart
+++ packages/data/lib/datasources/remote_datasource.dart
@@ -12,32 +12,32 @@ class RemoteDatasource {
 
   Future<AuthResponseDto> login(LoginDto login) async {
     final response = await httpClient.post<Map<String, dynamic>>('/auth/login', data: login.toJson());
-    return AuthResponseDto.fromJson(response.extra);
+    return AuthResponseDto.fromJson(response.data!);
   }
 
   Future<AuthResponseDto> register(RegisterDto register) async {
     final response = await httpClient.post<Map<String, dynamic>>('/auth/register', data: register.toJson());
-    return AuthResponseDto.fromJson(response.extra);
+    return AuthResponseDto.fromJson(response.data!);
   }
 
   Future<TodoDTO> createTodo(TodoDTO todo) async {
     final response = await httpClient.post<Map<String, dynamic>>('/todos', data: todo.toJson());
-    return TodoDTO.fromJson(response.extra);
+    return TodoDTO.fromJson(response.data!);
   }
 
   Future<TodoDTO> getTodoById(String id) async {
     final response = await httpClient.get<Map<String, dynamic>>('/todos/$id');
-    return TodoDTO.fromJson(response.extra);
+    return TodoDTO.fromJson(response.data!);
   }
 
   Future<List<TodoDTO>> getAllTodos() async {
     final response = await httpClient.get<List<dynamic>>('/todos');
-    return (response.extra as List).map((e) => TodoDTO.fromJson(e as Map<String, dynamic>)).toList();
+    return (response.data!).map((e) => TodoDTO.fromJson(e as Map<String, dynamic>)).toList();
   }
 
   Future<TodoDTO> updateTodoById(String id, TodoDTO todo) async {
     final response = await httpClient.put<Map<String, dynamic>>('/todos/$id', data: todo.toJson());
-    return TodoDTO.fromJson(response.extra);
+    return TodoDTO.fromJson(response.data!);
   }
 
   Future<void> deleteTodoById(String id) async {
