b196224ab01c8655dbe9bd972c94d7a06968b0e6
Dmitro Serdun <d.serdun@webtrit.com>
2024-06-03 00:44:42 +0300

    feat(data): add local keys datasource

--- packages/data/lib/di/injection.module.dart
+++ packages/data/lib/di/injection.module.dart
@@ -6,8 +6,14 @@
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'dart:async' as _i2;
 
-import 'package:data/di/injection.dart' as _i4;
-import 'package:dio/dio.dart' as _i3;
+import 'package:data/datasources/datasources.dart' as _i8;
+import 'package:data/datasources/local_keys_datasource.dart' as _i3;
+import 'package:data/datasources/remote_datasource.dart' as _i5;
+import 'package:data/di/injection.dart' as _i10;
+import 'package:data/repositories/auth_repository.dart' as _i9;
+import 'package:data/repositories/tasks_repository.dart' as _i7;
+import 'package:dio/dio.dart' as _i4;
+import 'package:domain/domain.dart' as _i6;
 import 'package:injectable/injectable.dart' as _i1;
 
 class DataPackageModule extends _i1.MicroPackageModule {
@@ -15,8 +21,16 @@ class DataPackageModule extends _i1.MicroPackageModule {
   @override
   _i2.FutureOr<void> init(_i1.GetItHelper gh) {
     final registerModule = _$RegisterModule();
-    gh.lazySingleton<_i3.Dio>(() => registerModule.githubClient());
+    gh.factory<_i3.LocalKeysDatasource>(() => _i3.LocalKeysDatasource());
+    gh.lazySingleton<_i4.Dio>(() => registerModule.githubClient());
+    gh.factory<_i5.RemoteDatasource>(() => _i5.RemoteDatasource(gh<_i4.Dio>()));
+    gh.factory<_i6.TaskRepository>(
+        () => _i7.TasksRepositoryImpl(gh<_i8.RemoteDatasource>()));
+    gh.factory<_i6.AuthRepository>(() => _i9.AuthRepositoryImpl(
+          gh<_i8.RemoteDatasource>(),
+          gh<_i3.LocalKeysDatasource>(),
+        ));
   }
 }
 
-class _$RegisterModule extends _i4.RegisterModule {}
+class _$RegisterModule extends _i10.RegisterModule {}
--- packages/data/lib/repositories/auth_repository.dart
+++ packages/data/lib/repositories/auth_repository.dart
@@ -4,20 +4,26 @@ import 'package:data/dtos/auth/auth.dart';
 import 'package:domain/domain.dart';
 
 import '../datasources/datasources.dart';
+import '../datasources/local_keys_datasource.dart';
 
 @Injectable(as: AuthRepository)
 class AuthRepositoryImpl implements AuthRepository {
-  AuthRepositoryImpl(this.remoteDatasource);
+  AuthRepositoryImpl(this.remoteDatasource, this.localKeysDatasource);
 
   final RemoteDatasource remoteDatasource;
+  final LocalKeysDatasource localKeysDatasource;
 
   @override
-  Future<void> login(String number, String password) {
-    return remoteDatasource.login(LoginDto(email: number, password: password));
+  Future<void> login(String number, String password) async {
+    final response = await remoteDatasource.login(LoginDto(email: number, password: password));
+    await localKeysDatasource.saveToken(response.token);
+    return Future.value();
   }
 
   @override
-  Future<void> register(String number, String password) {
-    return remoteDatasource.register(RegisterDto(email: number, password: password));
+  Future<void> register(String number, String password) async {
+    final response = await remoteDatasource.register(RegisterDto(email: number, password: password));
+    await localKeysDatasource.saveToken(response.token);
+    return Future.value();
   }
 }
