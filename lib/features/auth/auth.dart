a3b266ccfc1fbb270d2130339f5550cc370462c4
Dmitro Serdun <d.serdun@webtrit.com>
2024-06-02 22:06:53 +0300

    style: remove extra spaces

--- lib/features/auth/auth.dart
+++ lib/features/auth/auth.dart
@@ -1,5 +1,4 @@
+export 'consts.dart';
 export 'sign_in/sign_in.dart';
 export 'sign_up/sign_up.dart';
 export 'widgets/widgets.dart';
-export 'consts.dart';
-
--- packages/data/lib/data.dart
+++ packages/data/lib/data.dart
@@ -1,2 +1,1 @@
 export 'di/di.dart';
-
