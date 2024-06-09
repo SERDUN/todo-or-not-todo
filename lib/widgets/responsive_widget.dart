ecc8f8d437523cc512a11e088beaed33bfe7a6d8
Dmitro Serdun <d.serdun@webtrit.com>
2024-06-09 15:28:41 +0300

    feat: add responsive dependency and created base widget for type of platform

---
+++ lib/widgets/responsive_widget.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import 'package:responsive_framework/responsive_framework.dart';
+
+class ResponsiveWidget extends StatelessWidget {
+  const ResponsiveWidget({
+    super.key,
+    this.desktop,
+    this.tablet,
+    this.mobile,
+  });
+
+  final Widget? desktop;
+  final Widget? tablet;
+  final Widget? mobile;
+
+  @override
+  Widget build(BuildContext context) {
+    // Desktop
+    if (ResponsiveBreakpoints.of(context).isDesktop) {
+      return desktop ?? tablet ?? mobile!;
+    }
+
+    // Tablet
+    if (ResponsiveBreakpoints.of(context).isTablet) {
+      return tablet ?? desktop ?? mobile!;
+    }
+
+    // Mobile
+    return mobile ?? tablet ?? desktop!;
+  }
+}
